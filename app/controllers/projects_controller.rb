2d4ba40d273fca5740122bba1a65852f4f32ee9a
EvangelistaR <rafael.evans@live.com>
2025-02-04 01:54:47 -0300

    layout for projects session

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,5 @@
+class ProjectsController < ApplicationController
+  layout "admin"
+  before_action :authenticate_user!
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'projects/index'
   devise_for :users
   resources :dashboard, only: [:index]
+  resources :projects, only: [:index]
 
   root "home#index"
 end
